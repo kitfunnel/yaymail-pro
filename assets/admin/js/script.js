c8bf2c5620a88fd496b8fc014f7a6fef03ad7f2b
KitFunnel <kitfunnel@gmail.com>
2022-11-10 12:41:07 -0500

    Limpieza

--- assets/admin/js/script.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-document.body.style.overflow = "hidden";
-document.body.classList.add("yaymail-customizer-page-builder");
--- views/templates/emails/email-order-details-border-after.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined( 'ABSPATH' ) || exit;
-$sent_to_admin = ( isset( $sent_to_admin ) ? $sent_to_admin : false );
-$email         = ( isset( $email ) ? $email : '' );
-$plain_text    = ( isset( $plain_text ) ? $plain_text : '' );
